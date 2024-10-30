e6ee8bfefdca089799d8c533631d589e4b6faa19
Edo404 <edoardogamurrini@gmail.com>
2024-10-30 12:00:06 +0100

    starting mern

---
+++ server.js
@@ -0,0 +1,16 @@
+//const express = require('express')
+import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.post("/products", (req,res) => {});
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("server started at http://localhost:5000 ");
+})
+
